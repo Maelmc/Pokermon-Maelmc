7f073e41264b6d9fb5d3be9f5033955dfad62dbd
Maelmc <30575979+Maelmc@users.noreply.github.com>
2025-06-30 18:15:14 +0200

    Fixed Sun Stone rolling Ogerpon rank + put atlas in their own file

---
+++ maelmcsprites.lua
@@ -0,0 +1,160 @@
+SMODS.Atlas({
+  key = "modicon",
+  path = "icon.png",
+  px = 32,
+  py = 32
+}):register()
+
+SMODS.Atlas({
+  key = "Others-Maelmc",
+  path = "others.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Custom-Maelmc",
+  path = "Custom.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "shiny_Custom-Maelmc",
+  path = "ShinyCustom.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Mart-Maelmc",
+  path = "Mart.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "maelmcvouchers",
+  path = "maelmcvouchers.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Pokedex3-Maelmc",
+  path = "Pokedex3.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "shiny_Pokedex3-Maelmc",
+  path = "Shinydex3.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Pokedex4-Maelmc",
+  path = "Pokedex4.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "shiny_Pokedex4-Maelmc",
+  path = "Shinydex4.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Pokedex6-Maelmc",
+  path = "Pokedex6.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "shiny_Pokedex6-Maelmc",
+  path = "Shinydex6.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Pokedex8-Maelmc",
+  path = "Pokedex8.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "shiny_Pokedex8-Maelmc",
+  path = "Shinydex8.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Pokedex9-Maelmc",
+  path = "Pokedex9.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "shiny_Pokedex9-Maelmc",
+  path = "Shinydex9.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Regionals-Maelmc",
+  path = "Regionals.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "shiny_Regionals-Maelmc",
+  path = "ShinyRegionals.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Gmax-Maelmc",
+  path = "Gmax.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "shiny_Gmax-Maelmc",
+  path = "ShinyGmax.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "Megas-Maelmc",
+  path = "Megas.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "shiny_Megas-Maelmc",
+  path = "ShinyMegas.png",
+  px = 71,
+  py = 95
+}):register()
+
+SMODS.Atlas({
+  key = "pokedeck-Maelmc",
+  path = "pokedeck.png",
+  px = 71,
+  py = 95,
+}):register()
--- pokermon-maelmc.lua
+++ pokermon-maelmc.lua
@@ -33,175 +33,23 @@ table.insert(pokermon.family,{"gible","gabite","garchomp","mega_garchomp"})
 table.insert(pokermon.family,{"ogerpon","ogerpon_wellspring","ogerpon_hearthflame","ogerpon_cornerstone"})
 table.insert(pokermon.family,{"g_corsola","cursola"})
 
-SMODS.Atlas({
-  key = "modicon",
-  path = "icon.png",
-  px = 32,
-  py = 32
-}):register()
-
-SMODS.Atlas({
-  key = "Others-Maelmc",
-  path = "others.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Custom-Maelmc",
-  path = "Custom.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "shiny_Custom-Maelmc",
-  path = "ShinyCustom.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Mart-Maelmc",
-  path = "Mart.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "maelmcvouchers",
-  path = "maelmcvouchers.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Pokedex3-Maelmc",
-  path = "Pokedex3.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "shiny_Pokedex3-Maelmc",
-  path = "Shinydex3.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Pokedex4-Maelmc",
-  path = "Pokedex4.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "shiny_Pokedex4-Maelmc",
-  path = "Shinydex4.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Pokedex6-Maelmc",
-  path = "Pokedex6.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "shiny_Pokedex6-Maelmc",
-  path = "Shinydex6.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Pokedex8-Maelmc",
-  path = "Pokedex8.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "shiny_Pokedex8-Maelmc",
-  path = "Shinydex8.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Pokedex9-Maelmc",
-  path = "Pokedex9.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "shiny_Pokedex9-Maelmc",
-  path = "Shinydex9.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Regionals-Maelmc",
-  path = "Regionals.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "shiny_Regionals-Maelmc",
-  path = "ShinyRegionals.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Gmax-Maelmc",
-  path = "Gmax.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "shiny_Gmax-Maelmc",
-  path = "ShinyGmax.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "Megas-Maelmc",
-  path = "Megas.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "shiny_Megas-Maelmc",
-  path = "ShinyMegas.png",
-  px = 71,
-  py = 95
-}):register()
-
-SMODS.Atlas({
-  key = "pokedeck-Maelmc",
-  path = "pokedeck.png",
-  px = 71,
-  py = 95,
-}):register()
+--Load Sprites file
+local sprite, load_error = SMODS.load_file("maelmcsprites.lua")
+if load_error then
+  sendDebugMessage ("The error is: "..load_error)
+else
+  sprite()
+end
 
 SMODS.Rank {
-    key = 'Ogerpon',
-    card_key = 'O',
-    pos = { x = 0 },
-    nominal = 0,
-    in_pool = function(self)
-      return false
-    end,
+  key = 'Ogerpon',
+  card_key = 'O',
+  pos = { x = 0 },
+  nominal = 0,
+  weight = 0,
+  in_pool = function(self)
+    return false
+  end,
 }
 
 maelmc_config = SMODS.current_mod.config
