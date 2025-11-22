897796974ba7ab1ea1e312272c9f44ba055828ee
Maelmc <30575979+Maelmc@users.noreply.github.com>
2025-11-22 19:15:06 +0100

    Better pkrs code

--- stickers/stickers.lua
+++ stickers/stickers.lua
@@ -8,7 +8,7 @@ local pokerus =  {
     end,
     apply = function(self, card, val)
         card.ability[self.key] = val
-        if card.ability.extra then
+        if card.ability.extra and type(card.ability.extra) == "table" then
             G.E_MANAGER:add_event(Event({
                 trigger = "after",
                 time = 0.2,
@@ -16,18 +16,32 @@ local pokerus =  {
                     for _ = 1, energy_max + (G.GAME.energy_plus or 0) do
                         if can_increase_energy(card) then
                             energize(card,nil,nil,true)
-                            if card.ability.extra and type(card.ability.extra) == "table" then
-                                if card.ability.extra.c_energy_count then
-                                    card.ability.extra.c_energy_count = card.ability.extra.c_energy_count + 1
-                                else
-                                    card.ability.extra.c_energy_count = 1
-                                end
-                            else --if a base balatro joker
-                                if card.ability.c_energy_count then
-                                    card.ability.c_energy_count = card.ability.c_energy_count + 1
-                                else
-                                    card.ability.c_energy_count = 1
-                                end
+                            if card.ability.extra.c_energy_count then
+                                card.ability.extra.c_energy_count = card.ability.extra.c_energy_count + 1
+                            else
+                                card.ability.extra.c_energy_count = 1
+                            end
+                        else
+                            break
+                        end
+                    end
+                    play_sound('maelmc_pokerus_sound', 1, 0.2)
+                    card:juice_up(1, 0.5)
+                    return true
+                end
+            }))
+        else
+            G.E_MANAGER:add_event(Event({
+                trigger = "after",
+                time = 0.2,
+                func = function()
+                    for _ = 1, energy_max + (G.GAME.energy_plus or 0) do
+                        if can_increase_energy(card) then
+                            energize(card,nil,nil,true)
+                            if card.ability.c_energy_count then
+                                card.ability.c_energy_count = card.ability.c_energy_count + 1
+                            else
+                                card.ability.c_energy_count = 1
                             end
                         else
                             break
