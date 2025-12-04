f771e7761b843c371bb9bb2e774fd21d85992a35
Maelmc <30575979+Maelmc@users.noreply.github.com>
2025-12-04 12:35:06 +0100

    fixed swoobat sometimes not preventing debuff

--- functions/debuffs.lua
+++ functions/debuffs.lua
@@ -2,8 +2,8 @@
 
 -- return values: true, false, or 'prevent_debuff'
 SMODS.current_mod.set_debuff = function(card)
-   if card:is_suit("Hearts") and card:get_seal() == "Red" and next(find_joker("swoobat")) then return 'prevent_debuff' end
-   if card:get_id() and card:get_id() ~= 7 and next(find_joker("mega_barbaracle")) then return true end
+   if card:is_suit("Hearts",true) and card:get_seal(true) == "Red" and next(SMODS.find_card("j_maelmc_swoobat")) then return 'prevent_debuff' end
+   if card:get_id() and card:get_id() ~= 7 and next(SMODS.find_card("j_maelmc_mega_barbaracle")) then return true end
    return false
 end
 
